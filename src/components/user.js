f82f7dc91226859c27ae27bc2ea8786586f8584a
Harish1651999 <hk1651999@gmail.com>
2023-01-31 09:11:48 +0530

    add lifecycle methods.

--- src/components/user.js
+++ src/components/user.js
@@ -2,12 +2,11 @@ import classes from "./user.module.css";
 import React, { Component } from "react";
 
 class User extends Component {
+  componentWillUnmount() {
+    console.log("Users will unmounted!");
+  }
   render() {
-    return (
-      <li className={classes.user} key={this.props.key}>
-        {this.props.name}
-      </li>
-    );
+    return <li className={classes.user}>{this.props.name}</li>;
   }
 }
 
