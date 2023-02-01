a715d403566e89b259d7b604bef9462adf738f3f
Harish1651999 <hk1651999@gmail.com>
2023-02-01 08:22:23 +0530

    add usercontext.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import UserFinder from "./components/UserFinder";
+import UserContext from "./store/users-context";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
 
 function App() {
+  const userscontext = {
+    users: DUMMY_USERS,
+  };
+
   return (
-    <div>
+    <UserContext.Provider value={userscontext}>
       <UserFinder />
-    </div>
+    </UserContext.Provider>
   );
 }
 
